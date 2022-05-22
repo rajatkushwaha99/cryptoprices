341ccec23ab91ef5c5d3099c895443390b4f75f5
rajatkushwaha99 <rajatiit5032@gmail.com>
2022-05-22 20:40:15 +0530

    Test Run

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ function App() {
   return (
     <div className="coin-app">
       <div className="coin-search">
-        <h1 className='coin-text'>Search Any Currency</h1>
+        <h1 className='coin-text'>Search any currency</h1>
         <form>
           <input 
             className = "coin-input"
