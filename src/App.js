ca361da09a9fd06188fdcb66e84a7db889671195
rajatkushwaha99 <rajatiit5032@gmail.com>
2022-05-22 20:19:18 +0530

    Deploy changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,79 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import Coin from './Coin'
+
+// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+
+  document.title = "CryptoApp"
+  // useEffect(() => {
+    
+  // }, []);  
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+        axios
+        .get(
+          'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+        )
+        .then(res => {
+          setCoins(res.data);
+          console.log(res.data)
+          console.log("Data Fetched")
+        })
+        .catch(error => console.log(error));
+    },1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+
+  })
+  
+  
+  const handleChange = e => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter(coin => 
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className='coin-text'>Search Any Currency</h1>
+        <form>
+          <input 
+            className = "coin-input"
+            type="text" 
+            onChange={handleChange}
+            placeholder='Search' 
+          />
+        </form>
+      </div>
+      {filteredCoins.map(coin => {
+        return (
+          <Coin
+            key={coin.id}
+            name = {coin.name}
+            price = {coin.current_price}
+            symbol = {coin.symbol}
+            marketcap = {coin.total_volume}
+            volume = {coin.market_cap}
+            image = {coin.image}
+            priceChange = {coin.price_change_percentage_24h}         
+            
+          />
+        );
+      })}      
     </div>
   );
 }
 
 export default App;
+
---
+++ src/Coin.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import bootstrap from 'bootstrap';
+import './App.css';
+
+const Coin = ({
+    name,
+    price,
+    symbol,
+    marketcap,
+    volume,
+    image,
+    priceChange
+}) => {
+    return (
+        <div className="coin-container container" id='coincontainer'>
+            <div className="coin-row row">
+                <div className="coin col-4 row">
+                    <img src={image} alt="Crypto" className="col-2" id = "coin-image" />
+                    <h1 className="col-8 coin-name">{name}</h1>
+                    {/* <p className="coin-symbol col-2">{symbol}</p> */}
+                </div>
+
+                <div className="coin-data col-8 row">
+                    <p className="coin-price coindata col-2 ">${price}</p>
+                    <p className="coin-volume coindata col-2 ">${volume.toLocaleString()}</p>
+
+                    {priceChange < 0?(
+                        <p className="coin-percent-red coindata col-2 ">{priceChange.toFixed(2)}%</p>
+                    ) : (
+                        <p className="coin-percent-green coindata col-2 ">{priceChange.toFixed(2)}%</p>
+                    )}
+
+                    <p className="coin-marketcap coindata col-2 ">
+                        Mkt Cap: ${marketcap.toLocaleString()}
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Coin;
